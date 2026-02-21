33e7d147d1475fc56a9fe939ec1cd708b097538d
Soham Thapaliya <thapaliyasoham123@gmail.com>
2026-02-21 22:56:57 +0545

    updated

--- navbar/script.js
+++ navbar/script.js
@@ -1,4 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const sideMenu = document.getElementById("sideMenu");
+
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active"); // cross animation
+    sideMenu.classList.toggle("active");   // slide menu
 });
